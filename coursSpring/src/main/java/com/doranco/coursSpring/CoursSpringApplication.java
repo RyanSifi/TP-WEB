0d6ae2cf0560f34b8d736a8d2b295640aa875bb0
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-03-18 16:32:54 +0100

    Initial Commit

---
+++ coursSpring/src/main/java/com/doranco/coursSpring/CoursSpringApplication.java
@@ -0,0 +1,13 @@
+package com.doranco.coursSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursSpringApplication.class, args);
+	}
+
+}
