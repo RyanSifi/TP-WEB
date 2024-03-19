5e7761efb04becde23190719de54dacee1037cf8
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-03-19 10:25:03 +0100

    Routage

--- coursSpring/src/main/java/com/doranco/coursSpring/CoursSpringApplication.java
+++ coursSpring/src/main/java/com/doranco/coursSpring/CoursSpringApplication.java
@@ -2,6 +2,10 @@ package com.doranco.coursSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class CoursSpringApplication {
@@ -9,5 +13,13 @@ public class CoursSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoursSpringApplication.class, args);
 	}
+}
 
+@Controller
+class HomeController {
+	@GetMapping("/home/{title}")
+	public String home(@PathVariable String title, Model model){
+		model.addAttribute("title", title);
+		return "index";
+	}
 }
